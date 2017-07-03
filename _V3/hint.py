f52ba5f8e6fff468f7821b58eebe1c23dc4bce6d
Alberto Ramos <alberto.ramos@emc.com>
2017-07-03 21:44:45 +1000

    Add Python 3 versions

---
+++ _V3/hint.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import os
+for each_photo in os.listdir("photos"):
+    print ("Uploading " + str(each_photo))
+    src = os.path.join("photos", each_photo)
+    print ("full path to photo is : " + src)
+## Alterntively walk recursively a dir tree. It creates a string and 2 lists
+##
+##for (dirpath, dirnames, filenames) in os.walk("photos"):
+
